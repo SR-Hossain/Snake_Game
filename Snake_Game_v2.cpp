049065ca120a476778eef1a79e4c7d82010e7168
SR-Hossain <65627729+SR-Hossain@users.noreply.github.com>
2021-06-03 18:50:00 +0600

    Works on linux 😁

--- Snake_Game_v2.cpp
+++ Snake_Game_v2.cpp
@@ -8,6 +8,7 @@ struct winsize w;
 #define jjj ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
 
 
+
 // int pageno;
 
 int bs = 23, totalboard[500][500], point, fruitposx, fruitposy, h=1,stageunlocked[4], stagenumber=1;
@@ -67,12 +68,13 @@ char cs, ch;
 
 
 // Snake
-int snakeposx[1000], snakeposy[1000], snakelen = 9;
+int snakeposx[1000], snakeposy[1000], snakelen = 9, uur;
 bool f;
 
 
 
 vector<int> prevconsize;
+
 
 
 vector<int> consolemid();
@@ -87,11 +89,7 @@ void help(bool);
 void game(bool);
 void continue_game(bool);
 
-void stage_normal();
-void stage_campaign();
-void stage_tunnel();
-void stage_apartment();
-void stage_maze();
+void stages(int);
 void brdr(int);
 void Draw_border(int);
 
@@ -106,6 +104,7 @@ char retch();
 
 
 void Control(int);
+
 
 
 
@@ -136,6 +135,7 @@ void gotoXY(int row, int col, string str){
     int roww = row + changepos[0]/2 - 10;
     printf("\033[%d;%dH",roww,coll);
 	cout<<str;
+    gotoXY(bs+1,20);
 }
 void gotoXY(int row, int col, char str){
     vector<int> changepos = prevconsize = consolemid();
@@ -143,6 +143,7 @@ void gotoXY(int row, int col, char str){
     int roww = row + changepos[0]/2 - 10;
     printf("\033[%d;%dH",roww,coll);
 	cout<<str;
+    printf("\033[22;20H");
 }
 
 
@@ -185,18 +186,17 @@ void Control(int pos){
     if(pos == 3){   // Level
         level(1);
         char c = retch();
-        if(c == 'd')levelh++;
-        else if(c == 'a')levelh--;
-
-        if(levelh == -1)levelh = 0;
-        else if(levelh == 3)levelh = 2;
-
-        if(c == 'j'){
+        if(c == 'd')levelh+=levelh==2?0:1;
+        else if(c == 'a')levelh-=levelh==0?0:1;
+        else if(c == 'j'){
             level(0);
             Control(4);
             return;
         }
-
+        else{
+            Control(3);
+            return;
+        }
         level(0);
         Control(3);
     }
@@ -204,22 +204,17 @@ void Control(int pos){
         if(stageh==5)stageh=0;
         stage(1);
         char c = retch();
-        if(c == 'd' or c == 's')stageh++;
-        else if(c == 'a' or c == 'w')stageh--;
-
-        if(stageh == -1)stageh=4;
-        else if(stageh == 5)stageh=0;
-
-        if(c == 'j'){
+        if(c == 'd' or c == 's')stageh=stageh==4?0:stageh+1;
+        else if(c == 'a' or c == 'w')stageh=stageh==0?4:stageh-1;
+        else if(c == 'j'){
             stage(0);
-            brdr(0);
             game(0);
-            if(stageh == 0)stage_normal();
-            else if(stageh == 1)stage_campaign();
-            else if(stageh == 2)stage_tunnel();
-            else if(stageh == 3)stage_apartment();
-            else if(stageh == 4)stage_maze();
+
             game(1);
+            return;
+        }
+        else{
+            Control(4);
             return;
         }
         stage(0);
@@ -238,12 +233,11 @@ void Control(int pos){
         if(continuecontentpos == 4)continuecontentpos=0;
 
         if(c == 'j'){
-            continue_game(0); // Continue
+            continue_game(0);
             if(continuecontentpos == 0){
                 return;
             }
             else if(continuecontentpos == 1){ // New Game -> Level
-                game(0);
                 Control(3);
             }
             else if(continuecontentpos == 2){ // Level
@@ -265,60 +259,72 @@ void Control(int pos){
 
 }
 
+void stages(int key)
+{
+    if (key == 0)   // normal
+    {
+        for (int i = 0; i < bs; i++) gotoXY(i, 0, "|"), totalboard[i][0] = 3;
+        for (int i = 0; i < bs; i++) gotoXY(i, 2 * bs, "|"), totalboard[i][2 * bs] = 3;
+        for (int i = 0; i <= 2 * bs; i++) gotoXY(0, i, "_"), totalboard[0][i] = 3;
+        for (int i = 0; i <= 2 * bs; i++) gotoXY(bs - 1, i, "\'"), totalboard[bs - 1][i] = 3;
+    }
 
-void stage_normal(){
-    for(int i=0; i<bs; i++)gotoXY(i, 0, "|"), totalboard[i][0]=3;
-    for(int i=0; i<bs; i++)gotoXY(i, 2*bs, "|"),totalboard[i][2*bs]=3;
-    for(int i=0; i<=2*bs; i++)gotoXY(0,i, "_"),totalboard[0][i]=3;
-    for(int i=0; i<=2*bs; i++)gotoXY(bs-1,i, "\'"),totalboard[bs-1][i]=3;    
+    if (key == 1)   // Campaign
+    {
+        for (int i = 0; i <= 2 * bs; i++) gotoXY(0, i, border), totalboard[0][i] = 1;
+        for (int i = 0; i <= 2 * bs; i++) gotoXY(bs - 1, i, border), totalboard[bs - 1][i] = 1;
+        for (int i = 0; i < bs; i++) gotoXY(i, 0, border), totalboard[i][0] = 1;
+        for (int i = 0; i < bs; i++) gotoXY(i, 2 * bs, border), totalboard[i][2 * bs] = 1;
+    }
+
+    if (key == 2)   // Tunnel
+    { // **********************************************
+        stages(0);
+
+        string solid(10, border);
+        gotoXY(0, 0, solid);
+        gotoXY(bs - 1, 0, solid);
+        gotoXY(0, 2 * bs - 9, solid);
+        gotoXY(bs - 1, 2 * bs - 9, solid);
+        for (int i = 0; i < 10; i++) totalboard[0][i] = totalboard[bs - 1][i] = totalboard[0][2 * bs - 9 + i] = totalboard[bs - 1][2 * bs - 9 + i] = 1;
+
+        for (int i = 11; i <= bs + bs - 11; i++) gotoXY(6, i), cout << (char)border, totalboard[6][i] = 1;
+        for (int i = 11; i <= bs + bs - 11; i++) gotoXY(bs - 6, i), cout << (char)border, totalboard[bs - 6][i] = 1;
+    }
+
+    if (key == 3)   // Apartment
+    {
+        stages(0);
+        string s;
+        s += border;
+        for (int i = 0; i <= 2 * bs; i++) gotoXY(6, i, s), totalboard[6][i] = 1;
+        for (int i = 1; i <= 6; i++) gotoXY(i, 16, s), totalboard[i][16] = 1;
+        for (int i = 1; i <= 6; i++) gotoXY(i, 17, s), totalboard[i][17] = 1;
+        for (int i = 0; i <= 2 * bs; i++)
+            if (i < 13 or i > 19) gotoXY(bs - 6, i, s), totalboard[bs - 6][i] = 1;
+    }
+
+    if (key == 4)   // Maze
+    {
+        stages(3);
+        for (int i = bs - 6; i < bs; i++)
+            gotoXY(i, 20, (char)219), totalboard[i][20] = 1;
+        for (int i = bs - 6; i < bs; i++)
+            gotoXY(i, 21, (char)219), totalboard[i][21] = 1;
+        for (int i = 14; i < 30; i++)
+            gotoXY(bs - 1, i, (char)219), totalboard[bs - 1][i] = 1, totalboard[0][i] = 1;
+        for (int i = bs - 3; i < bs; i++)
+            gotoXY(i, 2 * bs - 1, (char)219), totalboard[i][2 * bs - 1] = 1;
+        for (int i = bs - 3; i < bs; i++)
+            gotoXY(i, 2 * bs, (char)219), totalboard[i][2 * bs] = 1, totalboard[i][0] = 1;
+    }
 }
-
-void stage_campaign(){
-    for(int i=0; i<=2*bs; i++)gotoXY(0,i, border), totalboard[0][i]=1;
-    for(int i=0; i<=2*bs; i++)gotoXY(bs-1,i, border),totalboard[bs-1][i]=1;
-    for(int i=0; i<bs; i++)gotoXY(i, 0, border),totalboard[i][0]=1;
-    for(int i=0; i<bs; i++)gotoXY(i, 2*bs, border),totalboard[i][2*bs]=1;
-}
-
-void stage_tunnel(){    // ********************************************** 
-    stage_normal();
-
-    string solid(10, border);
-    gotoXY(0,0,solid); gotoXY(bs-1,0,solid); gotoXY(0, 2*bs-9, solid); gotoXY(bs-1, 2*bs-9, solid);
-    for(int i=0; i<10; i++)totalboard[0][i] = totalboard[bs-1][i] = totalboard[0][2*bs-9+i] = totalboard[bs-1][2*bs-9+i] = 1;
-
-    for(int i=11; i<=bs+bs-11; i++)gotoXY(6, i), cout<<(char)border, totalboard[6][i] = 1;
-    for(int i=11; i<=bs+bs-11; i++)gotoXY(bs-6, i), cout<<(char)border, totalboard[bs-6][i] = 1;
-}
-
-void stage_apartment(){
-    stage_normal();
-    string s; s+=border;
-    for(int i=0; i<=2*bs; i++)gotoXY(6, i, s), totalboard[6][i]=1;
-    for(int i=1; i<=6; i++)gotoXY(i, 16, s), totalboard[i][16]=1;
-    for(int i=1; i<=6; i++)gotoXY(i, 17, s), totalboard[i][17]=1;
-    for(int i=0; i<=2*bs; i++)
-    if(i<13 or i>19) gotoXY(bs-6, i, s), totalboard[bs-6][i]=1;
-    
-}
-
-void stage_maze(){
-    stage_apartment();
-    for(int i=bs-6; i<bs; i++)gotoXY(i, 20, (char)219), totalboard[i][20]=1;
-    for(int i=bs-6; i<bs; i++)gotoXY(i, 21, (char)219), totalboard[i][21]=1;
-    for(int i=14; i<30; i++)gotoXY(bs-1, i, (char)219), totalboard[bs-1][i]=1, totalboard[0][i]=1;
-    for(int i=bs-3; i<bs; i++)gotoXY(i, 2*bs-1, (char)219), totalboard[i][2*bs-1]=1;
-    for(int i=bs-3; i<bs; i++)gotoXY(i, 2*bs, (char)219), totalboard[i][2*bs]=1, totalboard[i][0]=1;
-    
-}
-
-
 
 void snake(int key){
     if(key == 1){
-        for(int i=0, j=0; i<snakelen-1; i++, j+=2){
+        for(int i=0; i<snakelen-1; i++){
             if(i&1)gotoXY(snakeposx[i], snakeposy[i], "o");
-            else gotoXY(snakeposx[i], snakeposy[i], (char)248);
+            else gotoXY(snakeposx[i], snakeposy[i], "ơ");
         }
         gotoXY(snakeposx[snakelen-1], snakeposy[snakelen -1], "O");
     }
@@ -328,13 +334,11 @@ void snake(int key){
         }
     }
     else{
-        snakelen = 9;
-        for(int i=0, j=0; i<snakelen; i++, j+=2){
+        for(int i=0, j=0; i<9; i++, j+=2){
             snakeposx[i] = 10;
             snakeposy[i] = 16+j;
             totalboard[10][16+j] = 1;
         }
-        totalboard[10][34] = 0;
     }
 }
 
@@ -363,7 +367,8 @@ void fruit(int key){
 
 
 void move_snake(int x, int y){
-    snakeposx[snakelen] = x; snakeposy[snakelen] = y;
+    snakeposx[snakelen] = x;
+    snakeposy[snakelen] = y;
 
     totalboard[snakeposx[0]][snakeposy[0]] = 0;
     totalboard[x][y] = 1;
@@ -394,10 +399,11 @@ void fruit_eaten(int x, int y){
 }
 
 void key_press(){
-    int x=snakeposx[snakelen-1], y=snakeposy[snakelen-1];
-    if(_kbhit()) 
-    cs=retch();
-
+    int x = snakeposx[snakelen-1];
+    int y = snakeposy[snakelen-1];
+    if(_kbhit()){
+        cs=retch();
+    }
     if(!(cs=='a' or cs=='s' or cs=='d' or cs=='w' or cs=='j'))cs = ch;
 
     else if(cs+ch==234 or cs+ch==197)cs=ch;
@@ -413,15 +419,19 @@ void key_press(){
     }
     else ch = cs;
 
+
     if(cs=='w')x--;
     else if(cs=='s')x++;
     else if(cs=='a')y-=2;
     else if(cs=='d')y+=2;
 
-
-    if(totalboard[x][y] == 1){
+    if(uur and x==10 and y==34){
+        move_snake(x,y);
+        uur = 0;
+    }
+    else if(totalboard[x][y] == 1){
         gotoXY(snakeposx[snakelen-1], snakeposy[snakelen-1], 'X');
-        gameover = 1;
+        gameover=1;
     }
     else if(totalboard[x][y] == 2)fruit_eaten(x,y);
     else if(totalboard[x][y] == 3){
@@ -429,18 +439,22 @@ void key_press(){
         else if(x == bs-1)x=1;
         else if(y == 0)y=2*bs-2;
         else if(y == 2*bs)y=2;
-
         if(totalboard[x][y] == 2)fruit_eaten(x,y);
         else move_snake(x,y);
     }
     else move_snake(x,y);
-    gotoXY(bs+1,20);
+    gotoXY(-1,-1);
 }
 
 void game(bool key){
     if(key){
+        game(0);
+        stages(stageh);
         cs = 'd';
-        point = gameover = 0;
+        point = 0;
+        gameover = 0;
+        snakelen = 9;
+        uur=1;
         snake(3);
         fruit(3);
         while(gameover == 0){
@@ -450,7 +464,7 @@ void game(bool key){
             fruit(1);
             key_press();
             if(etg)return;
-            usleep(130000);
+            usleep((2-levelh)*20000+80000);
             snake(0);
             fruit(0);
             gotoXY(-2, 20);cout<<"                 ";
@@ -462,8 +476,8 @@ void game(bool key){
         gotoXY(14, 10, "Press x to go to next level");
         gameover = 0;
         point = 0;
-        
-        for(cs = retch(); cs-'j' and cs-'x'; cs=retch());
+        cs = retch();
+        for(;cs!='j' and cs!='x'; cs=retch());
         gotoXY(10, 10);cout<<"                              ";
         gotoXY(12, 10);cout<<"                           ";
         gotoXY(14, 10);cout<<"                              ";
@@ -474,7 +488,7 @@ void game(bool key){
     else{
         snake(0);
         fruit(0);
-        for(int i=0; i<=21; i++)for(int j=0; j<=50; j++)totalboard[i][j]=0;
+        for(int i=0; i<=22; i++)for(int j=0; j<=50; j++)totalboard[i][j]=0;
     }
 }
 
@@ -508,18 +522,10 @@ void brdr(int key){
             return;
         }
 
-        if(stageh == 0)stage_normal();
-        else if(stageh == 1)stage_campaign();
-        else if(stageh == 2)stage_tunnel();
-        else if(stageh == 3)stage_apartment();
-        else if(stageh == 4)stage_maze();
+        stages(stageh);
     }
     else if(key == 3){
-        if(stageh == 0)stage_normal();
-        else if(stageh == 1)stage_campaign();
-        else if(stageh == 2)stage_tunnel();
-        else if(stageh == 3)stage_apartment();
-        else if(stageh == 4)stage_maze();
+        stages(stageh);
 
     }
 }
